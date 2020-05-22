9ebe8ae14dd1f795c95633bec719f85fa2cd607a
Didier Cuetia <chato1337@gmail.com>
2020-05-21 23:55:53 -0500

    ajustes de localizacion

--- public/js/app.js
+++ public/js/app.js
@@ -91897,8 +91897,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/didiercuetia/Sites/sigeex/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/didiercuetia/Sites/sigeex/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\sigeex\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\sigeex\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
